ddf8c27a89bf341f49c37621f1e094ecef9e934d
mademoisellegeek42@gmail.com <mademoisellegeek42@gmail.com@bdfe6cc6-c051-6e6c-3a9d-ed7aa84c502f>
2012-01-20 10:26:44 +0000

    implémentation fonctions grille

--- trunk/IntelligenceArtificielle/src/com/mademoisellegeek/ia/Grille.java
+++ trunk/IntelligenceArtificielle/src/com/mademoisellegeek/ia/Grille.java
@@ -4,25 +4,37 @@ public class Grille {
 
     private int lignes;
     private int colonnes;
+    private int xDepart;
+    private int yDepart;
+    private int[][] humains;
+    private int[][] vampires;
+    private int[][] loups;
 
     public Grille(int nbLignes, int nbColonnes) {
         this.lignes = nbLignes;
         this.colonnes = nbColonnes;
+        this.humains = new int[nbLignes][nbColonnes];
+        this.vampires = new int[nbLignes][nbColonnes];
+        this.loups = new int[nbLignes][nbColonnes];
+        //TODO remplir les tableaux de zéro
     }
 
-    void setCaseDepart(int xDepart, int xArrivee) {
-        throw new UnsupportedOperationException("Not yet implemented");
+    void setCaseDepart(int xDepart, int yDepart) {
+        this.xDepart = xDepart;
+        this.yDepart = yDepart;
     }
 
     void ajouterHumain(int xHumain, int yHumain) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        this.humains[xHumain][yHumain]++;
     }
 
     void vider() {
-        throw new UnsupportedOperationException("Not yet implemented");
+        //tODO mettre humains, vampires et loups à zéro
     }
 
     void update(int xCase, int yCase, int nbHumains, int nbVampires, int nbLoupsGarous) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        this.humains[xCase][yCase] = nbHumains;
+        this.vampires[xCase][yCase] = nbVampires;
+        this.loups[xCase][yCase] = nbLoupsGarous;
     }
 }
