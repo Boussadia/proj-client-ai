104ce185181b944fcb188d5f91c75c57a9ad55fb
mademoisellegeek42@gmail.com <mademoisellegeek42@gmail.com@bdfe6cc6-c051-6e6c-3a9d-ed7aa84c502f>
2012-02-25 06:59:23 +0000

    algorithme alpha beta et structures de recherche missing files

---
+++ IntelligenceArtificielle/src/com/mademoisellegeek/ia/Deplacement.java
@@ -0,0 +1,17 @@
+package com.mademoisellegeek.ia;
+
+/**
+ *
+ * @author cbaldock
+ */
+class Deplacement {
+
+    private int nbIndividus;
+    private int xDepart;
+    private int yDepart;
+    private int xArrivee;
+    private int yArrivee;
+    
+    //TODO
+    
+}
---
+++ IntelligenceArtificielle/src/com/mademoisellegeek/ia/Tour.java
@@ -0,0 +1,18 @@
+package com.mademoisellegeek.ia;
+
+/**
+ *
+ * @author cbaldock
+ */
+public class Tour {
+    
+    Deplacement[] deplacements = new Deplacement[2];
+    int xCible;
+    int yCible;
+    boolean attaque;
+    Grille resultat;
+    int evaluation;
+    
+    //TODO
+    
+}
