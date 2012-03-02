0974e06d91ba632246220e63dad5d8b626d33f53
ahmedboussadia <ahmedboussadia@bdfe6cc6-c051-6e6c-3a9d-ed7aa84c502f>
2012-03-02 09:23:39 +0000

    on enleve le fichier config.txt du repo a cause de conflits entre les noms d'equipes pour les tests

--- IntelligenceArtificielle/src/com/mademoisellegeek/ia/AI.java
+++ IntelligenceArtificielle/src/com/mademoisellegeek/ia/AI.java
@@ -5,7 +5,7 @@ public class AI {
     public AI() {}
     
     public static void jouer(Grille grille) {
-        grille.makePerfectMove(14);//TODO tester profondeurs ???
+        grille.makePerfectMove(1);//TODO tester profondeurs ???
         return;
     }
 }
