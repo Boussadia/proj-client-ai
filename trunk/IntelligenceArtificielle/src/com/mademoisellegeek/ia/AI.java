0cfe39dd118bb12ed5ec0f27150cde5a2e82c92a
mademoisellegeek42@gmail.com <mademoisellegeek42@gmail.com@bdfe6cc6-c051-6e6c-3a9d-ed7aa84c502f>
2012-03-02 08:46:11 +0000

    déterminer tour vampires/loups

--- trunk/IntelligenceArtificielle/src/com/mademoisellegeek/ia/AI.java
+++ trunk/IntelligenceArtificielle/src/com/mademoisellegeek/ia/AI.java
@@ -5,7 +5,7 @@ public class AI {
     public AI() {}
     
     public static void jouer(Grille grille) {
-        grille.makePerfectMove(2);//TODO profondeur ???
+        grille.makePerfectMove(14);//TODO tester profondeurs ???
         return;
     }
 }
